3c46d24f6cd924e5fc4dd96a45f3b108de7d7fd9
Miroljub Grujin <miroljub.grujin@bridgemaker.com>
2023-12-13 15:14:28 +0100

    build bug fixed

--- mocks/navigation/index.ts
+++ mocks/navigation/index.ts
@@ -1,3 +1,72 @@
+export const megaMenuData = [
+  {
+    title: 'Produkte',
+    menu: [
+      [
+        {
+          title: 'LOREM',
+          url: '',
+        },
+        {
+          title: 'Über die türex',
+          url: '/lorem-page',
+        },
+      ],
+      [
+        {
+          title: 'MOREM',
+          url: '',
+        },
+        {
+          title: 'Häufig gestellte Fragen',
+          url: '/faq',
+        },
+      ],
+    ],
+  },
+  {
+    title: 'Unternehmen',
+    menu: [
+      [
+        {
+          title: 'Über uns',
+          url: '/about-us',
+        },
+        {
+          title: 'Unser Partner türex',
+          url: '/about-us',
+        },
+        {
+          title: 'Showroom',
+          url: '/showroom',
+        },
+        {
+          title: 'Karriere',
+          url: 'https://türex.at',
+        },
+      ],
+      [
+        {
+          title: 'Info & Hilfe',
+          url: '/contact',
+        },
+        {
+          title: 'Kontakt',
+          url: '/contact',
+        },
+        {
+          title: 'FAQ',
+          url: '/faq',
+        },
+        {
+          title: 'Lieferung',
+          url: '/shipping-conditions',
+        },
+      ],
+    ],
+  },
+];
+
 export const navigationData = [
   {
     title: 'Home',
