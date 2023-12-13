1f1c18d0078f7b6c445c7b7b17500c25a8e6ffe2
Miroljub Grujin <miroljub.grujin@bridgemaker.com>
2023-12-13 23:36:30 +0100

    fix build

--- components/organisms/price-table/table/index.tsx
+++ components/organisms/price-table/table/index.tsx
@@ -12,43 +12,48 @@ const TableContent = ({ query }: TableContentProps) => {
     (table: any) => table.title.toLowerCase() === query.toLowerCase()
   );
 
-  return filteredTableData.map((table: PriceItem, index: number) => {
-    return (
-      <div key={index} className="p-4 bg-white rounded-lg mt-8">
-        <div className="flex gap-2">
-          <div className="flex items-start mt-.5">
-            <table.icon className="w-6 text-primary-500" />
+  return (
+    <div>
+      {filteredTableData.map((table: PriceItem, index: number) => {
+        return (
+          <div key={index} className="p-4 bg-white rounded-lg mt-8">
+            <div className="flex gap-2">
+              <div className="flex items-start mt-.5">
+                <table.icon className="w-6 text-primary-500" />
+              </div>
+              <div>
+                <UI.Typography variant="h5" className="font-bold">
+                  {table.title}
+                </UI.Typography>
+                <UI.Typography variant="xs" className="leading-5">
+                  {table.workingHours}
+                </UI.Typography>
+              </div>
+            </div>
+            <div className="mt-8">
+              <UI.Typography variant="h5" className="font-bold">
+                Preise
+              </UI.Typography>
+              <table className="w-full">
+                <tbody>
+                  {table.services.map((el: any, index: number) => (
+                    <tr
+                      key={index}
+                      className="border-b border-[#E2E8F0] h-[56px] text-lg"
+                    >
+                      <td>{el.service}</td>
+                      <td>{el.price}€</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div>
-            <UI.Typography variant="h5" className="font-bold">
-              {table.title}
-            </UI.Typography>
-            <UI.Typography variant="xs" className="leading-5">
-              {table.workingHours}
-            </UI.Typography>
-          </div>
-        </div>
-        <div className="mt-8">
-          <UI.Typography variant="h5" className="font-bold">
-            Preise
-          </UI.Typography>
-          <table className="w-full">
-            <tbody>
-              {table.services.map((el: any, index: number) => (
-                <tr
-                  key={index}
-                  className="border-b border-[#E2E8F0] h-[56px] text-lg"
-                >
-                  <td>{el.service}</td>
-                  <td>{el.price}€</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  });
+        );
+      })}
+      ;
+    </div>
+  );
 };
 
 export default TableContent;
