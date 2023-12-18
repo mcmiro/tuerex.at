5669a60a8f26fa2f2dedeac072d342a8511f1518
Miroljub Grujin <miroljub.grujin@bridgemaker.com>
2023-12-18 21:54:21 +0100

    favicon added + sitemap dependency installed and sitemap should generate a static sitemap.xml file

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'http://localhost:3000' || 'https://www.tuerex.at',
+  generateRobotsTxt: true,
+};
--- next-sitemap.config.ts
+++
@@ -1,8 +0,0 @@
-import { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
-  siteUrl: 'http://localhost:3000' || 'https://www.tuerex.at',
-  generateRobotsTxt: true,
-};
-
-export default config;
